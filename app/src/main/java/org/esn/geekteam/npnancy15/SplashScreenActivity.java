f49be137f36b4f154f01aeadc17cc3773d2f2db4
JeremieSamson <jeremie.samson76@gmail.com>
2015-04-28 14:11:44 +0200

    Add splashscreen

---
+++ app/src/main/java/org/esn/geekteam/npnancy15/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package org.esn.geekteam.npnancy15;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by Spider on 28/04/15.
+ */
+
+public class SplashScreenActivity extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
